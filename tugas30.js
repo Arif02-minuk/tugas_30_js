bf400ad63d52beb767b93031e5a7f9dd4abbae14
arif wijanarko <arifpethod@gmail.com>
2023-06-16 10:19:53 +0700

    tugas javascript 30
    untuk niomic

---
+++ tugas30.js
@@ -0,0 +1,7 @@
+function regex(value) {
+    let callData = value.match(/[es]/g);
+
+    console.log(callData);
+}
+
+regex("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
